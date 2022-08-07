2ad3d5e4381de5566d369bc7e20cf13c1e8c2427
CMitch213 <110313496+CMitch213@users.noreply.github.com>
2022-08-07 05:46:22 -0400

    Update v1.2
    allows custom size

--- Code/main.py
+++ Code/main.py
@@ -1,7 +1,23 @@
-# stars v1.1
+# stars v1.2
+inp = ""
 stars = ""
-while stars != "*******************************************************":
-    stars = stars + "*"
-    print(stars)
-    if stars == "*******************************************************":
-        break
+num: int = 0
+amntStars: int = 0
+try:
+    inp = input("Type how large you want the base to be:")
+except:
+    print("Error, failed to accept your input")
+num = inp
+
+try:
+    while int(num) >= amntStars:
+        stars = stars + "*"
+        print(stars)
+        amntStars = amntStars + 1
+        if int(num) == amntStars:
+            break
+except:
+    print("ERROR: prolly an invalid number")
+
+
+
